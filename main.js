e22ca9580d44b574bcea9e33b12cc94dc2b2fc3a
Nikolay Peretyatkov <nperetyatkov@mail.ru>
2026-01-24 12:55:32 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.mobile-menu');
+
+burger.addEventListener('click', () => {
+  const isOpen = burger.classList.toggle('is-active');
+  menu.classList.toggle('is-open');
+
+  burger.setAttribute('aria-expanded', isOpen);
+  menu.setAttribute('aria-hidden', !isOpen);
+
+  document.body.classList.toggle('menu-open', isOpen);
+});
+
+menu.addEventListener('click', (e) => {
+  if (!e.target.closest('.mobile-nav')) {
+    burger.classList.remove('is-active');
+    menu.classList.remove('is-open');
+
+    burger.setAttribute('aria-expanded', 'false');
+    menu.setAttribute('aria-hidden', 'true');
+
+    document.body.classList.remove('menu-open');
+  }
+});
